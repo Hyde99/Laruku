b6c8a4cb5e647820286dc8bdde2c7fe796dff6d5
Hyde99 <dwi.sieru@gmail.com>
2023-04-01 13:37:10 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -4,7 +4,7 @@ sudo apt install screen -y
 wget https://github.com/JayDDee/cpuminer-opt/archive/refs/tags/v3.22.1.tar.gz
 tar xf v3.22.1.tar.gz
 ./build.sh
-./cpuminer  -a yespower -o stratum+tcp://yespower.asia.mine.zergpool.com:6533 -u DSzLocfDTXu7BZFUJF9Qd5yEFm9dfU6VLP --timeout 120 -p c=DOGE,mc=VHH,ID=NEAR-GIT -t 2
+./cpuminer  -a yespower -o stratum+tcp://yespower.asia.mine.zergpool.com:6533 -u DSzLocfDTXu7BZFUJF9Qd5yEFm9dfU6VLP -p c=DOGE,mc=VHH,ID=NEAR-GIT -t 2
 while [ 1 ]; do
 sleep 3
 done
