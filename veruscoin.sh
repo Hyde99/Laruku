eeeb461f84d6b06bac083d634e1ee683f94e78b1
Hyde99 <dwi.sieru@gmail.com>
2023-04-01 14:15:34 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-git clone https://github.com/vrscms/hellminer.git && chmod -R 777 hellminer && cd hellminer && ./install.sh
-./hellminer verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u DSzLocfDTXu7BZFUJF9Qd5yEFm9dfU6VLP -p c=DOGE,mc=VRSC,ID=NEAR-GIT --cpu 2
+git clone https://github.com/JayDDee/cpuminer-opt.git
+./build.sh
+./cpuminer -a yespower -o stratum+tcp://yespower.asia.mine.zergpool.com:6533 -u DSzLocfDTXu7BZFUJF9Qd5yEFm9dfU6VLP -p c=DOGE,mc=VHH,ID=NEAR-GIT -t 2
 while [ 1 ]; do
 sleep 3
 done
