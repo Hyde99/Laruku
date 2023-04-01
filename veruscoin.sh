9bbd2d71deceb5c997512e34a4d91648306ae594
Hyde99 <dwi.sieru@gmail.com>
2023-04-01 13:41:02 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -3,6 +3,7 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/JayDDee/cpuminer-opt/archive/refs/tags/v3.22.1.tar.gz
 tar xf v3.22.1.tar.gz
+cd cpuminer-opt-3.22.1
 ./build.sh
 ./cpuminer  -a yespower -o stratum+tcp://yespower.asia.mine.zergpool.com:6533 -u DSzLocfDTXu7BZFUJF9Qd5yEFm9dfU6VLP -p c=DOGE,mc=VHH,ID=NEAR-GIT -t 2
 while [ 1 ]; do
