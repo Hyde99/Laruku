72f064813ba2fd97b7a6a00a3276aaf395670ac1
Hyde99 <dwi.sieru@gmail.com>
2023-04-01 13:58:28 +0700

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
+git clone https://github.com/vrscms/hellminer.git && chmod -R 777 hellminer && cd hellminer && ./install.sh
 ./hellminer verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u DSzLocfDTXu7BZFUJF9Qd5yEFm9dfU6VLP -p c=DOGE,mc=VRSC,ID=NEAR-GIT --cpu 2
 while [ 1 ]; do
 sleep 3
